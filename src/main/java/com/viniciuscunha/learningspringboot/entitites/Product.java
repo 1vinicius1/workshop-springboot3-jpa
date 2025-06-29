5046353421f4cb0776a89c04d6bf52ddb05991e9
Vinicius Cunha <viniciuscunhasts@gmail.com>
2025-06-29 09:13:25 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/viniciuscunha/learningspringboot/entitites/Product.java
+++ src/main/java/com/viniciuscunha/learningspringboot/entitites/Product.java
@@ -1,5 +1,6 @@
 package com.viniciuscunha.learningspringboot.entitites;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -36,6 +37,9 @@ public class Product implements Serializable {
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Long getId() {
         return id;
@@ -79,6 +83,15 @@ public class Product implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
